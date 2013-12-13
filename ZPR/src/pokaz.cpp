6de096a72f6223c4eedced77e4f580ea08c87099
redhead89 <danielrogulski89@gmail.com>
2013-12-13 18:21:24 +0100

    read files

--- ZPR/src/pokaz.cpp
+++ ZPR/src/pokaz.cpp
@@ -6,13 +6,33 @@
  */
 #include <iostream>
 #include "pokaz.h"
+#include <string>
+#include <fstream>
+using namespace std;
 
+void pokaza() {
 
-void pokaza(){
-	using namespace std;
-	cout<<"to ja !!!!!"<<endl;
+	ofstream fout;
+	ifstream fin;
+	//fout.open("jara.txt");
 
+	string line;
+
+	//fout<<"ala ma kota";
+	//fout.close();
+
+	fin.open("jara.txt");
+	//getline(fin,line);
+	char ch;
+	int i = 0;
+	while (fin.get(ch)) {
+		cout << ch;
+		i++;
+		if(i%30==0){cout<<endl;}
+	}
+
+	cout << "to ja !!!!!" << endl;
+	cout << line << endl;
+	cout << "wczytalem" << endl;
 }
-
-
 
